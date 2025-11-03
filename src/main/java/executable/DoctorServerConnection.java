468e51d678689ad8da740b8f2d3afc0a0d71c991
carmeencaballero <carmen.caballeroherreros@usp.ceu.es>
2025-11-03 20:05:40 +0100

    Creación DoctorServerConnection.java

---
+++ src/main/java/executable/DoctorServerConnection.java
@@ -0,0 +1,12 @@
+package executable;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+
+
+public class DoctorServerConnection {
+
+}
